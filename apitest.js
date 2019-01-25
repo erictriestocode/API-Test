0ab09283e7265619140ea77a1670c57731ce1941
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-01-24 23:36:39 -0600

    Adding files

---
+++ apitest.js
@@ -0,0 +1,46 @@
+// API Key: AIzaSyARUTkixy00DWI57qipoe4H6_kNASXlqdQ 
+
+
+// {
+//     "destination_addresses" : [ "Detroit, MI, USA" ],
+//     "origin_addresses" : [ "Chicago, IL, USA" ],
+//     "rows" : [
+//        {
+//           "elements" : [
+//              {
+//                 "distance" : {
+//                    "text" : "281 mi",
+//                    "value" : 452812
+//                 },
+//                 "duration" : {
+//                    "text" : "4 hours 19 mins",
+//                    "value" : 15558
+//                 },
+//                 "status" : "OK"
+//              }
+//           ]
+//        }
+//     ],
+//     "status" : "OK"
+//  }
+
+var queryURL = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyARUTkixy00DWI57qipoe4H6_kNASXlqdQ";
+
+console.log(queryURL);
+
+$.ajax ({
+    url: queryURL,
+    method: "GET"
+})
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+
+        // Log the queryURL
+        console.log(queryURL);
+
+        // Log the resulting object
+        console.log(response);
+
+        // Transfer content to HTML
+       
+    });
